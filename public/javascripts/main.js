356b083e45f398033f886bd946c429654d9da0fb
Romain Laï-King <romain@homeloop.fr>
2018-11-21 16:10:54 +0100

    SSE response handler

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,6 @@
 (function(){
+
+  
   const input = document.getElementById('input')
 
   const submitBtn = document.getElementById('submit-btn')
@@ -10,10 +12,19 @@
     resultDiv.textContent = result
   }
 
+  const id = Math.trunc(Math.random() * 1000000)
+  const  evtSource = new EventSource(`/subscribe?id=${id}`)
+  evtSource.addEventListener('itor', function(e){
+    refreshResult(e.data)
+  })
+
   form.addEventListener('submit', e => {
     e.preventDefault()
-    fetch(`/itor?value=${input.value}`).then( result => result.text())
-    .then(refreshResult)
+    fetch(`/itor?value=${input.value}`, {
+      headers: {
+        'x-client-id': id
+      }
+    })
     .catch(error => {
       console.error(error)
     })
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,53 @@
 const express = require('express');
 const router = express.Router();
-
 const itor = require('../services/itor')
-/* GET home page. */
-router.get('/itor', function(req, res, next) {
-  res.set({
-    'Content-Type': 'text/plain'
+
+
+const subscribers = new Map()
+
+class Subscription{
+  constructor(id, res){
+    this.id = id
+    this.res = res
+    subscribers.set(id, this)
+  }
+
+  emit(event, data){
+    this.res.write('event: ' + event + '\n' )
+    this.res.write('data: ' + data + '\n\n')
+  }
+
+  end(){
+    subscribers.delete(this.id)
+  }
+
+
+}
+
+router.get('/subscribe', (req, res) => {
+
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive'
   })
-  res.send(itor(parseInt(req.query.value)))
-});
+
+  const subscription = new Subscription(req.query.id, res)
+  req.on("close", function() {
+    subscription.end()
+  })
+})
+
+
+router.get('/itor', (req, res) => {
+  const subscription = subscribers.get(req.headers['x-client-id'])
+  if(subscription){
+    setTimeout(() => {
+      subscription.emit('itor', itor(parseInt(req.query.value)))
+    }, 2000)
+    return res.status(202).end()
+  }
+  res.status(204).end()
+})
 
 module.exports = router;
