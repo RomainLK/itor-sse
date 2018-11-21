cac16ca0b168db23f3f02e656c8d48ea9b6fa910
Romain Laï-King <romain@homeloop.fr>
2018-11-21 13:51:34 +0100

    itor function

---
+++ services/itor.js
@@ -0,0 +1,46 @@
+function decompose(int, divider){
+  return { time: Math.trunc(int / divider), rest: int % divider }
+}
+function repeat(symbol, time){
+
+  let result = ''
+  for(let i = 0; i < time; i++){
+    result += symbol
+  }
+  return result
+}
+
+module.exports = function itor(int){
+  if(int%1 !== 0){
+    throw new TypeError('Number is not an integer')
+  }
+  if(int < 1){
+    throw new RangeError('Number is inferior to 1')
+  }
+  if(int > 100){
+    throw new RangeError('Number is superior to 100')
+  }
+  let result = ''
+
+
+  function symbol(int, symbol, inferiorSymbol, value, inferiorValue){
+    let { time, rest} =  decompose(int, value)
+    const substractMode = rest >= (value - inferiorValue)
+    if(substractMode){
+      result += inferiorSymbol + symbol
+      rest = rest - (value-inferiorValue)
+    }
+    if(time < 4){
+      result += repeat(symbol, time)
+    }
+    return rest
+  }
+  debugger
+  const hundredR = symbol(int, 'C', 'X', 100, 10)
+  const fiftyR = symbol(hundredR, 'L', 'X', 50, 10)
+  const tenR = symbol(fiftyR, 'X', 'I', 10, 1)
+  const fiveR = symbol(tenR, 'V', 'I', 5, 1)
+  symbol(fiveR, 'I', '', 1, 0)
+  
+  return result
+}
---
+++ tests/itor.spec.js
@@ -0,0 +1,69 @@
+const itor = require('../services/itor')
+
+describe('itor', () => {
+  test('It throws on non integer', () => {
+    expect(() => itor(10.5)).toThrowError('Number is not an integer')
+  })
+
+  test('It throws on integer inferior to 1', () => {
+    expect(() => itor(0)).toThrowError('Number is inferior to 1')
+  })
+
+  test('It throws on integer superior to 100', () => {
+    expect( () => itor(101)).toThrowError('Number is superior to 100')
+  })
+
+  test('It converts 100', () => {
+    expect(itor(100)).toEqual('C')
+  })
+
+  test('It converts 50', () => {
+    expect(itor(50)).toEqual('L')
+  })
+
+  test('It converts 1', () => {
+    expect(itor(1)).toEqual('I')
+  })
+
+  test('It converts 3', () => {
+    expect(itor(3)).toEqual('III')
+  })
+
+  test('It converts 4', () => {
+    expect(itor(4)).toEqual('IV')
+  })
+
+  test('It converts 5', () => {
+    expect(itor(5)).toEqual('V')
+  })
+  test('It converts 9', () => {
+    expect(itor(9)).toEqual('IX')
+  })
+  test('It converts 10', () => {
+    expect(itor(10)).toEqual('X')
+  })
+
+  test('It converts 13', () => {
+    expect(itor(13)).toEqual('XIII')
+  })
+
+  test('It converts 14', () => {
+    expect(itor(14)).toEqual('XIV')
+  })
+
+  test('It converts 95', () => {
+    expect(itor(95)).toEqual('XCV')
+  })
+
+  test('It converts 90', () => {
+    expect(itor(90)).toEqual('XC')
+  })
+
+  test('It converts 99', () => {
+    expect(itor(99)).toEqual('XCIX')
+  })
+
+  test('It converts 88', () => {
+    expect(itor(88)).toEqual('LXXXVIII')
+  })
+})
